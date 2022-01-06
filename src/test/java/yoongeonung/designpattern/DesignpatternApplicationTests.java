b1e66ca98acf2bbd1ecb678af419d6d7e5d55543
yoongeonung <yoongeonung@gmail.com>
2022-01-06 15:01:40 +0900

    Refresh project

---
+++ src/test/java/yoongeonung/designpattern/DesignpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package yoongeonung.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
